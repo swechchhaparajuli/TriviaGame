f34a7a397e0fdb57dc8438d79c3a22a5e8e96fdf
swechchhaparajuli <swechchha@gmail.com>
2018-11-18 11:29:10 -0800

    presentation complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,24 @@ var cattleBool = false;
 var polarBool = false; 
 var oceanBool = false; 
 var ans = "";
+var qCount = 0;
+var questions = [];
+var answers = [];
+points = 0;
 
 
 var deforestation = {
 
-    q1: "_____ is the leading cause of deforestation",
-    q2: "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
-    q3: "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
-    q4: "The rate of deforestation equals to loss of _______ football field(s) every minute.",
-    q5: "___________ of the world’s tropical forests has already been cleared.",
-    q6: "The United States has less than ____ of the world’s population but consumes more than ______ of the world’s paper.",
-    q7: "_____ of the world’s oxygen is produced in the Amazon forest.",
+    q: [
+    "_____ is the leading cause of deforestation", 
+     "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
+     "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
+     "The rate of deforestation equals to loss of _______ football field(s) every minute.",
+     "___________ of the world’s tropical forests has already been cleared.",
+     "The United States has less than ____ of the world’s population but consumes more than ______ of the world’s paper.",
+     "_____ of the world’s oxygen is produced in the Amazon forest."
+
+    ],
 
       a1 : ["Agriculture"],
       a2 : ["Loss of forests"],
@@ -27,31 +34,51 @@ var deforestation = {
       a7 : ["20%"],
 
 
-    setData : function(){
-        deforestationBool = true; 
-        $("#question").text(this.q1); 
-        for (var i = 0; i < this.a1.length; i++ ){
-            var option = $("<button>");
-            option.addClass("answer");
-            option.attr("value", this.a1[i]);
-            option.text(this.a1[i]);
-            $(".answer").append(option);
-            console.log(this.a1[i]);
+      setData : function(){
+        ans = this.a1[0];
+        answers.push(this.a1);
+        answers.push(this.a2);
+        answers.push(this.a3);
+        answers.push(this.a4);
+        answers.push(this.a5);
+        answers.push(this.a6);
+        answers.push(this.a7);
+        console.log("answers");
+        console.log(answers[0][0]);
+
+        
+        for (var i = 0; i < 7; i++){
+            questions.push(this.q[i]);
         }
 
+        deforestationBool = true; 
+        $("#question").text(this.q[0]); 
+        
+        for (var i = 0; i < 4; i++ ){
+            
+            var option = $("<button>");
+            option.addClass("answer");
+            option.attr("value", answers[qCount][i]);
+            option.text(answers[0][i]);
+            $(".choices").append(option);
+        }  
+        qCount = 1;
     }
 
 };
 
 var cattle = {
     
-    q1: "A report from the World Resources Institute (WRI) in 1998, found that around ______ of coral reefs are threatened by human activity.",
-    q2: "According to a study done by the University of Georgia, ________ pounds of plastic trash winds up in our oceans each year.",
-    q3: "Ocean pollutions kills more than ________ sea birds each year.",
-    q4: "Approximately every square mile of ocean has more than _______ of plastic floating in it.",
-    q5: "There are about _____ dead zones in the ocean, which covers a similar size as the United Kingdom.",
-    q6: "There’s enough plastic in the ocean to circle the Earth _____ times.",
-    q7: "_______ are the most common element found in the ocean today.",
+    q: [
+        "_____ is the leading cause of deforestation", 
+         "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
+         "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
+         "The rate of deforestation equals to loss of _______ football field(s) every minute.",
+         "___________ of the world’s tropical forests has already been cleared.",
+         "The United States has less than ____ of the world’s population but consumes more than ______ of the world’s paper.",
+         "_____ of the world’s oxygen is produced in the Amazon forest."
+    
+        ],
     
       a1 : ["Agriculture"],
       a2 : ["Loss of forests"],
@@ -61,32 +88,51 @@ var cattle = {
       a6 : ["5% , 30%"],
       a7 : ["20%"],
 
-    setData : function(){
-        cattleBool = true; 
-        $("#question").text(this.q1); 
-        console.log(this.q1);
-        for (var i = 0; i < this.a1.length; i++ ){
-            var option = $("<button>");
-            option.addClass("answer");
-            option.attr("value", this.a1[i]);
-            option.text(this.a1[i]);
-            $(".answer").append(option);
-            console.log(this.a1[i]);
+      setData : function(){
+        ans = this.a1[0];
+        answers.push(this.a1);
+        answers.push(this.a2);
+        answers.push(this.a3);
+        answers.push(this.a4);
+        answers.push(this.a5);
+        answers.push(this.a6);
+        answers.push(this.a7);
+        console.log("answers");
+        console.log(answers[0][0]);
+
+        
+        for (var i = 0; i < 7; i++){
+            questions.push(this.q[i]);
         }
 
+        cattleBool = true; 
+        $("#question").text(this.q[0]); 
+        
+        for (var i = 0; i < 4; i++ ){
+            
+            var option = $("<button>");
+            option.addClass("answer");
+            option.attr("value", answers[qCount][i]);
+            option.text(answers[0][i]);
+            $(".choices").append(option);
+        }  
+        qCount = 1;
     }
 
 };
 
 var polar = {
     
-    q1: "A report from the World Resources Institute (WRI) in 1998, found that around ______ of coral reefs are threatened by human activity.",
-    q2: "According to a study done by the University of Georgia, ________ pounds of plastic trash winds up in our oceans each year.",
-    q3: "Ocean pollutions kills more than ________ sea birds each year.",
-    q4: "Approximately every square mile of ocean has more than _______ of plastic floating in it.",
-    q5: "There are about _____ dead zones in the ocean, which covers a similar size as the United Kingdom.",
-    q6: "There’s enough plastic in the ocean to circle the Earth _____ times.",
-    q7: "_______ are the most common element found in the ocean today.",
+    q: [
+        "_____ is the leading cause of deforestation", 
+         "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
+         "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
+         "The rate of deforestation equals to loss of _______ football field(s) every minute.",
+         "___________ of the world’s tropical forests has already been cleared.",
+         "The United States has less than ____ of the world’s population but consumes more than ______ of the world’s paper.",
+         "_____ of the world’s oxygen is produced in the Amazon forest."
+    
+        ],
     
      a1 : ["Agriculture"],
      a2 : ["Loss of forests"],
@@ -97,32 +143,50 @@ var polar = {
      a7 : ["20%"],
 
 
-    setData : function(){
-        polarBool = true; 
-        $("#question").text(this.q1); 
-        console.log(this.q1);
-        for (var i = 0; i < this.a1.length; i++ ){
-            var option = $("<button>");
-            option.addClass("answer");
-            option.attr("value", this.a1[i]);
-            option.text(this.a1[i]);
-            $(".answer").append(option);
-            console.log(this.a1[i]);
+     setData : function(){
+        ans = this.a1[0];
+        answers.push(this.a1);
+        answers.push(this.a2);
+        answers.push(this.a3);
+        answers.push(this.a4);
+        answers.push(this.a5);
+        answers.push(this.a6);
+        answers.push(this.a7);
+        console.log("answers");
+        console.log(answers[0][0]);
+
+        
+        for (var i = 0; i < 7; i++){
+            questions.push(this.q[i]);
         }
 
+        polarBool = true; 
+        $("#question").text(this.q[0]); 
+        
+        for (var i = 0; i < 4; i++ ){
+            
+            var option = $("<button>");
+            option.addClass("answer");
+            option.attr("value", answers[qCount][i]);
+            option.text(answers[0][i]);
+            $(".choices").append(option);
+        }  
+        qCount = 1;
     }
 
 };
 
 var oceans = {
 
-    q1: "A report from the World Resources Institute (WRI) in 1998, found that around ______ of coral reefs are threatened by human activity.",
-    q2: "According to a study done by the University of Georgia, ________ pounds of plastic trash winds up in our oceans each year.",
-    q3: "Ocean pollutions kills more than ________ sea birds each year.",
-    q4: "Approximately every square mile of ocean has more than _______ of plastic floating in it.",
-    q5: "There are about _____ dead zones in the ocean, which covers a similar size as the United Kingdom.",
-    q6: "There’s enough plastic in the ocean to circle the Earth _____ times.",
-    q7: "_______ are the most common element found in the ocean today.",
+    q: ["A report from the World Resources Institute (WRI) in 1998, found that around ______ of coral reefs are threatened by human activity.",
+     "According to a study done by the University of Georgia, ________ pounds of plastic trash winds up in our oceans each year.",
+     "Ocean pollutions kills more than ________ sea birds each year.",
+     "Approximately every square mile of ocean has more than _______ of plastic floating in it.",
+     "There are about _____ dead zones in the ocean, which covers a similar size as the United Kingdom.",
+     "There’s enough plastic in the ocean to circle the Earth _____ times.",
+     "_______ are the most common element found in the ocean today."
+
+    ],
     
      a1 : ["85%", "20%" , "5%"],
      a2 : ["18 billion"],
@@ -133,26 +197,43 @@ var oceans = {
      a7 : ["Plastics"],
 
    
-    setData : function(){
-        oceansBool = true; 
-        $("#question").text(this.q1); 
-        console.log(this.q1);
-        for (var i = 0; i < this.a1.length; i++ ){
-            var option = $("<button>");
-            option.addClass("answer");
-            //option.attr("", this.a1[i]);
-            option.text(this.a1[i]);
-            $(".answer").append(option);
-            console.log(this.a1[i]);
+     setData : function(){
+        ans = this.a1[0];
+        answers.push(this.a1);
+        answers.push(this.a2);
+        answers.push(this.a3);
+        answers.push(this.a4);
+        answers.push(this.a5);
+        answers.push(this.a6);
+        answers.push(this.a7);
+        console.log("answers");
+        console.log(answers[0][0]);
+
+        
+        for (var i = 0; i < 7; i++){
+            questions.push(this.q[i]);
         }
 
+        oceansBool = true; 
+        $("#question").text(this.q[0]); 
+        
+        for (var i = 0; i < 4; i++ ){
+            
+            var option = $("<button>");
+            option.addClass("answer");
+            option.attr("value", answers[qCount][i]);
+            option.text(answers[0][i]);
+            $(".choices").append(option);
+        }  
+        qCount = 1;
     }
 };
 
 $("#Ice_Caps").on("click", function() {
    
     $('body').css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/iceCap.jpg)"); 
-    $("#notBegun").css("visibility", "collapse"); 
+    $("#notBegun").empty();
+    //$("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
     polar.setData();
 
@@ -160,7 +241,8 @@ $("#Ice_Caps").on("click", function() {
 
 $("#Deforestation").on("click", function() {
     $('body').css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/deforestation.jpg)"); 
-    $("#notBegun").css("visibility", "collapse"); 
+    $("#notBegun").empty();
+    //$("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
     deforestation.setData();
 
@@ -168,7 +250,8 @@ $("#Deforestation").on("click", function() {
 
 $("#Agriculture").on("click", function() {
     $('body').css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/cattle.jpg)"); 
-    $("#notBegun").css("visibility", "collapse"); 
+    $("#notBegun").empty();
+   // $("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
     cattle.setData();
     
@@ -177,23 +260,57 @@ $("#Agriculture").on("click", function() {
 
 $("#Ocean").on("click", function() {
     $("body").css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/coral.jpg)"); 
-    $("#notBegun").css("visibility", "collapse"); 
+    $("#notBegun").empty();
+    //$("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
     oceans.setData();
 
 
 });
 
-$("#answer").on("click", function() {
-    
+$(".answer").on("click", function() {
+    console.log("something");
     
 
 });
 
 
 $("#submit").on("click", function() {
+    $(".choices").empty();
+
+    console.log(questions[qCount]);
+    console.log(qCount)
+
+    computeQuestions();
+    points++;
 
 });
+
+
+function computeQuestions(){
+
+    if(qCount > 6){
+        $(".started").empty();
+    }
+
+    $("#question").text(questions[qCount]); 
+    ans = answers[qCount][i];
+
+    for (var i = 0; i < answers[qCount].length; i++ ){
+        var option = $("<button>");
+        option.addClass("answer");
+        option.attr("value", answers[qCount][i]);
+        option.text(answers[qCount][i]);
+        $(".choices").append(option);
+        
+    }
+    qCount++;
+
+}
+
+function subTime(){
+
+}
 
 
 
