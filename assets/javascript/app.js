d203b395e5a1faf6e787553691c91bfbdfac05bb
swechchhaparajuli <swechchha@gmail.com>
2018-11-19 20:07:17 -0800

    images and functionality, only thing left is randomizing order of answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,22 @@
 
 
-var time = 300;
-var deforestationBool = false;
-var cattleBool = false; 
-var polarBool = false; 
-var oceanBool = false; 
+var time = 120;
+var triviaTheme = "";
 var ans = "";
 var qCount = 0;
 var questions = [];
 var answers = [];
 var points = 0;
 var intervalId;
+var option;
+var unAnswered = 0;
+var timeOut;
 
 var deforestation = {
 
     q: [
     "_____ is the leading cause of deforestation", 
-     "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
+     "___________ contribute(s) between 12-17 percent of annual global greenhouse gas emissions.",
      "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
      "The rate of deforestation equals to loss of _______ football field(s) every minute.",
      "___________ of the world’s tropical forests has already been cleared.",
@@ -24,17 +24,16 @@ var deforestation = {
      "_____ of the world’s oxygen is produced in the Amazon forest."
     ],
 
-      a1 : ["Agriculture"],
-      a2 : ["Loss of forests"],
-      a3 : ["100 years", "10 years", "35 years", ],
+      a1 : ["Agriculture", "pollution", "bykill", "oil spills"],
+      a2 : ["Loss of forests", "oil spills", "cars", "all of the above"],
+      a3 : ["100 years", "10 years", "35 years","1,000 years" ],
       a4 : ["20", "1", "3", "80"],
       a5 : ["50%", "25%", "83%", "12%"],
-      a6 : ["5% , 30%"],
-      a7 : ["20%"],
+      a6 : ["5% , 30%", "50%, 3%", "20%, 3%", "2%, 30%"],
+      a7 : ["20%", "95%", "25%", "65%"],
 
 
       setData : function(){
-        ans = this.a1[0];
         answers.push(this.a1);
         answers.push(this.a2);
         answers.push(this.a3);
@@ -42,27 +41,28 @@ var deforestation = {
         answers.push(this.a5);
         answers.push(this.a6);
         answers.push(this.a7);
-        console.log("answers");
-        console.log(answers[0][0]);
+       
 
         
         for (var i = 0; i < 7; i++){
             questions.push(this.q[i]);
         }
 
-        deforestationBool = true; 
+        triviaTheme = "deforestation";
         $("#question").text(this.q[0]); 
         
         for (var i = 0; i < 4; i++ ){
             
-            var option = $("<button>");
+            option = $("<button>");
             option.addClass("answer");
             option.attr("value", answers[qCount][i]);
             option.text(answers[0][i]);
             $(".choices").append(option);
-            console.log(option);
+           
         }  
         qCount = 1;
+
+        console.log(ans);
 
         intervalId = setInterval(subTime, 1000); 
     }
@@ -73,25 +73,23 @@ var cattle = {
     
     q: [
         "_____ is the leading cause of deforestation", 
-         "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
-         "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
-         "The rate of deforestation equals to loss of _______ football field(s) every minute.",
-         "___________ of the world’s tropical forests has already been cleared.",
-         "The United States has less than ____ of the world’s population but consumes more than ______ of the world’s paper.",
-         "_____ of the world’s oxygen is produced in the Amazon forest."
+         "It takes more than _______ gallons of water to produce 1 pound of meat",
+        "Animals raised for food produce approximately _____ times as much excrement as the entire human population",
+        "Raising animals for food now uses a staggering _____ of the Earth’s land mass.",
+        "Of all the agricultural land in the U.S., ______ is used to raise animals for food and grow grain to feed them",
+        "The world’s cattle alone consume a quantity of food equal to the caloric needs of ________ people—more than the entire human population on Earth",
+        "_____ football fields’ worth of land is bulldozed every minute to create more room for farmed animals and the crops that feed them."
+    ],
     
-        ],
-    
-      a1 : ["Agriculture"],
-      a2 : ["Loss of forests"],
-      a3 : ["100 years", "10 years", "35 years", ],
-      a4 : ["20", "1", "3", "80"],
-      a5 : ["50%", "25%", "83%", "12%"],
-      a6 : ["5% , 30%"],
-      a7 : ["20%"],
+      a1 : ["Agriculture", "pollution", "bykill", "oil spills"],
+      a2 : ["2,400", "200", "22,000", "50"],
+      a3 : ["130", "20", "100", "30"],
+      a4 : ["30%", "20%", "90%", "65%"],
+      a5 : ["80%","30%", "20%", "90%"],
+      a6 : ["8.7 billion", "200 million", "22,000", "50 billion" ],
+      a7 : ["Seven", "Eight", "Ninety", "45"],
 
       setData : function(){
-        ans = this.a1[0];
         answers.push(this.a1);
         answers.push(this.a2);
         answers.push(this.a3);
@@ -99,26 +97,27 @@ var cattle = {
         answers.push(this.a5);
         answers.push(this.a6);
         answers.push(this.a7);
-        console.log("answers");
-        console.log(answers[0][0]);
-
+       
+        
         
         for (var i = 0; i < 7; i++){
             questions.push(this.q[i]);
         }
 
-        cattleBool = true; 
+        triviaTheme = "cattle";
         $("#question").text(this.q[0]); 
         
         for (var i = 0; i < 4; i++ ){
             
-            var option = $("<button>");
+            option = $("<button>");
             option.addClass("answer");
             option.attr("value", answers[qCount][i]);
             option.text(answers[0][i]);
             $(".choices").append(option);
         }  
         qCount = 1;
+
+        console.log(ans);
 
         intervalId = setInterval(subTime, 1000); 
     }
@@ -129,26 +128,24 @@ var polar = {
     
     q: [
         "_____ is the leading cause of deforestation", 
-         "___________ contributes between 12 percent and 17 percent of annual global greenhouse gas emissions.",
-         "If the current rate of deforestation continues, it will take less than _______ years to destroy all the rainforests on the earth.",
-         "The rate of deforestation equals to loss of _______ football field(s) every minute.",
-         "___________ of the world’s tropical forests has already been cleared.",
-         "The United States has less than ____ of the world’s population but consumes more than ______ of the world’s paper.",
-         "_____ of the world’s oxygen is produced in the Amazon forest."
+        "In the last 100 years, the Environmental Protection Agency reported that the sea level has risen by ______ inches",
+        "Most major cities are in low lying coastal areas. They will be inundated when sea levels rise more than _____ feet.",
+        "According to NASA, the polar ice caps are melting at an alarming rate of ____ per decade.",
+        "The thickness of the Arctic Ice has decreased by ______ since the 1960s.",
+        "According to scientists at the U.S Center for Atmospheric Research, if the current rate of global temperature rise continues, the Arctic will be free of Ice by ______",
+        "Warmer ocean temperatures will mean ______ frequent and _______ intense weather events."
+    ],
     
-        ],
-    
-     a1 : ["Agriculture"],
-     a2 : ["Loss of forests"],
-     a3 : ["100 years"],
-     a4 : ["20"],
-     a5 : ["50%"],
-     a6 : ["5% , 30%"],
-     a7 : ["20%"],
+     a1 : ["Agriculture", "pollution", "bykill", "oil spills"],
+     a2 : ["6-8", "2-3", "15-16", "100-200"],
+     a3 : ["6", "2", "1", "32"],
+     a4 : ["9%", "29%", "90%", "20%"],
+     a5 : ["40%", "20%", "9%", "85%"],
+     a6 : ["2040", "2010", "2030", "2100"],
+     a7 : ["more, more", "less, more", "more, less", "less, less"],
 
 
      setData : function(){
-        ans = this.a1[0];
         answers.push(this.a1);
         answers.push(this.a2);
         answers.push(this.a3);
@@ -156,15 +153,14 @@ var polar = {
         answers.push(this.a5);
         answers.push(this.a6);
         answers.push(this.a7);
-        console.log("answers");
-        console.log(answers[0][0]);
+      
 
         
         for (var i = 0; i < 7; i++){
             questions.push(this.q[i]);
         }
 
-        polarBool = true; 
+        triviaTheme = "polar";
         $("#question").text(this.q[0]); 
         
         for (var i = 0; i < 4; i++ ){
@@ -176,6 +172,8 @@ var polar = {
             $(".choices").append(option);
         }  
         qCount = 1;
+
+        console.log(ans);
 
         intervalId = setInterval(subTime, 1000); 
     }
@@ -189,22 +187,21 @@ var oceans = {
      "Ocean pollutions kills more than ________ sea birds each year.",
      "Approximately every square mile of ocean has more than _______ of plastic floating in it.",
      "There are about _____ dead zones in the ocean, which covers a similar size as the United Kingdom.",
-     "There’s enough plastic in the ocean to circle the Earth _____ times.",
-     "_______ are the most common element found in the ocean today."
+     "There’s enough plastic in the ocean to circle the Earth _____ time(s).",
+     "_______ is the most common element found in the ocean today."
 
     ],
     
-     a1 : ["85%", "20%" , "5%"],
-     a2 : ["18 billion"],
-     a3 : ["one million"],
-     a4 : ["45,000"],
-     a5 : ["500"],
-     a6 : ["4"],
-     a7 : ["Plastics"],
+     a1 : ["85%", "20%" , "5%", "9%"],
+     a2 : ["18 billion", "200", "18 million", "200,000"],
+     a3 : ["one million", "100", "100,000", "20"],
+     a4 : ["45,000", "600", "20", "1 million" ],
+     a5 : ["500", "8", "96", "1 million"],
+     a6 : ["4", "300", "1", "560"],
+     a7 : ["Plastic", "coral", "fish", "oil"],
 
    
      setData : function(){
-        ans = this.a1[0];
         answers.push(this.a1);
         answers.push(this.a2);
         answers.push(this.a3);
@@ -212,15 +209,14 @@ var oceans = {
         answers.push(this.a5);
         answers.push(this.a6);
         answers.push(this.a7);
-        console.log("answers");
-        console.log(answers[0][0]);
+        
 
         
         for (var i = 0; i < 7; i++){
             questions.push(this.q[i]);
         }
 
-        oceansBool = true; 
+        triviaTheme = "oceans"; 
         $("#question").text(this.q[0]); 
         
         for (var i = 0; i < 4; i++ ){
@@ -233,13 +229,15 @@ var oceans = {
         }  
         qCount = 1;
 
+        console.log(ans);
+
         intervalId = setInterval(subTime, 1000); 
     }
 };
 
 $("#Ice_Caps").on("click", function() {
    
-    $('body').css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/iceCap.jpg)"); 
+    $('body').css("background-image", "url(./assets/images/iceCap.jpg)"); 
     $("#notBegun").empty();
     //$("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
@@ -248,7 +246,7 @@ $("#Ice_Caps").on("click", function() {
 });
 
 $("#Deforestation").on("click", function() {
-    $('body').css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/deforestation.jpg)"); 
+    $('body').css("background-image", "url(./assets/images/deforestation.jpg)"); 
     $("#notBegun").empty();
     //$("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
@@ -257,7 +255,7 @@ $("#Deforestation").on("click", function() {
 });
 
 $("#Agriculture").on("click", function() {
-    $('body').css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/cattle.jpg)"); 
+    $('body').css("background-image", "url(./assets/images/cattle.jpg)"); 
     $("#notBegun").empty();
    // $("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
@@ -267,19 +265,18 @@ $("#Agriculture").on("click", function() {
 });
 
 $("#Ocean").on("click", function() {
-    $("body").css("background-image", "url(file:///Users/Swechacha/Desktop/Bootcamp/week3/TriviaGame/assets/images/coral.jpg)"); 
+    $("body").css("background-image", "url(./assets/images/coral.jpg)"); 
     $("#notBegun").empty();
     //$("#notBegun").css("visibility", "collapse"); 
     $(".started").css("visibility", "visible"); 
     oceans.setData();
-
-
 });
 
 
 $("#submit").on("click", function() {
     resetTime();
     $("#countDown").text ("Time Left: " + timeConverter(0));
+    winOrLoss();
     computeQuestions();
 
 });
@@ -289,53 +286,113 @@ $("#submit").on("click", function() {
 
 function computeQuestions(){
     $(".choices").empty();
-
-    console.log(questions[qCount]);
-    console.log(qCount);
-
-
+    
+    console.log(ans);
     if(qCount > 6){
         $(".started").empty();
+
+        $(".started").append("<p> You got " + points + " correct.<p>");
+        $(".started").append("<p> You got " + (7 - (points + unAnswered)) + " incorrect.<p>");
+        $(".started").append("<p> You left " + unAnswered + " unanswered.<p>");
+
+        if (points > 3){
+            $(".started").append("<p> Okay, you kinda know your environmental stuff <p>");
+        }else{
+            $(".started").append("<p> You really need to study up on climate change! <p>");
+            
+        }
+
+      //  console.log(qCount);
+        restart = $("<button>");
+        restart.addClass("restart");
+        restart.text("Replay");
+        $(".started").append(restart);
     }
 
     $("#question").text(questions[qCount]); 
-    ans = answers[qCount][i];
+    
 
     for (var i = 0; i < answers[qCount].length; i++ ){
-        var option = $("<button>");
+        option = $("<button>");
+        
         option.addClass("answer");
         option.attr("value", answers[qCount][i]);
         option.text(answers[qCount][i]);
-        $(".choices").append(option);
-        
+        $(".choices").append(option);  
     }
     qCount++;
-
-    console.log(option)
-
     intervalId = setInterval(subTime, 1000); 
+
+   // console.log(option);
+
+    
 
 }
 
-$(".answer").on("click", function() {
-    console.log("something worked");
-    var image = $("<img>");
-    if (ans == this.value){
-        $(".started").css("visibility", "collapse");
-        image.attr("src", "url()");
-        intervalId = setInterval(($(".started").append(image)), 120); 
-        resetTime();
-        points++;
-    }else{
-        $(".started").css("visibility", "collapse");
-        image.attr("src", "url()");
-        intervalId = setInterval(($(".started").append(image)), 120); 
-        resetTime();
-    } 
+$(document).on("click", ".answer", function() {
+    ans = this.value;
 
-    computeQuestions();
+ //   computeQuestions(); //take this out if you want to be able to change your mind about answers
 
 });
+
+function winOrLoss(){
+    
+    var image = $("<img>");
+
+ //   console.log("ans: " + ans);
+ //   console.log("answers[qCount][0]:" + answers[qCount-1][0] );
+    if (ans == answers[qCount-1][0]){
+        points++;
+        $(".started").css("visibility", "collapse");
+        image.addClass("winOrLoss");
+        image.attr("src", "./assets/images/" +  triviaTheme +  "Win.jpg");
+        image.attr("height", "400px");
+        image.attr("width", "400px");
+       // intervalId = setInterval(($(".started").append(image)), 120); 
+       $("#notBegun").text("Yay! You got that right!");
+
+       $("#notBegun").append("<p> You have gotten " + points + " right so far <p>");
+
+       $("#notBegun").append(image);
+       timeOut = setTimeout(contGame,2500);
+
+      //  resetTime();
+        
+    }else if ( ans == ""){
+
+        unAnswered++;
+
+    }else{
+      $(".started").css("visibility", "collapse");
+        //image.addClass("winOrLoss");
+        image.attr("src", "./assets/images/" + triviaTheme + "Loss.jpg");
+        image.attr("height", "400px");
+        image.attr("width", "400px");
+        $("#notBegun").text("Sucks! The answer was " + answers[qCount-1][0]);
+        $("#notBegun").append("<p> You have gotten " + points + " right so far <p>");
+        $("#notBegun").append(image);
+        timeOut = setTimeout( contGame,2500);
+
+       // intervalId = setInterval(contGame, 120); 
+       // resetTime();
+    }
+    ans = ""; 
+}
+
+$(document).on("click", ".restart", function() {
+    location.reload();
+
+});
+
+
+
+function contGame(){
+    $(".started").css("visibility", "visible");
+    $("#notBegun").empty();
+    clearTimeout(timeOut);
+    
+}
 
 
 function subTime(){
@@ -350,7 +407,7 @@ function subTime(){
 
 function resetTime(){
     clearInterval(intervalId);
-    time = 300;
+    time = 120;
 }
 
 
@@ -358,6 +415,7 @@ function resetTime(){
 
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
+    
 
     if (seconds < 10) {
       seconds = "0" + seconds;
